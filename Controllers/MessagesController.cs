6e38eeddce834fc1ea3a71648cf3169d97c0bef9
Rachel S. Harness <rsharness@sep.com>
2022-06-08 11:48:55 -0400

    chat

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 namespace hotwire_demo.Controllers;
 public class MessagesController : Controller {
+  static public List<String> messages = new List<String>();
   [HttpGet("~/messages")]
-  public IActionResult Index() {
-    Console.WriteLine("Hello world");
-    return View();
+  public IActionResult Index(string message) {
+    Console.WriteLine(message);
+    messages.Add(message);
+    return View(messages);
   }
 }
